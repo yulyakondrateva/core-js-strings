41e3def5f90cfa013011586d9dd1f40aa21ffc35
Yulya Kondrateva <kond.yulya@gmail.com>
2023-11-20 00:10:23 +0300

    feat: implement 1 task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -253,8 +253,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const min = minutes.toString().padStart(2, '0');
+  const sec = seconds.toString().padStart(2, '0');
+  return `${min}:${sec}`;
 }
 
 /**
